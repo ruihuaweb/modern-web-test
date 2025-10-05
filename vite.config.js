ce3f9f1aebfdcd2f1d5acb6b7e6e5a752fbe5b41
ruihuaweb <ruihuamain@gmail.com>
2025-10-05 14:47:43 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/modern-web-test/'   // 👈 這裡加上你的 repo 名稱
+  base: '/modern-web-test/',   //repo 名稱
+  server:{
+    host: '0.0.0.0',
+    port:5000
+  }
 })
